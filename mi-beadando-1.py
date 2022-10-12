e1622f1fd030d9b36d16fd38c8baf27704a3c0a8
Plasius <Plasius@users.noreply.github.com>
2022-10-12 17:07:26 +0200

    Add files via upload

---
+++ mi-beadando-1.py
@@ -0,0 +1,25 @@
+
+input_file = open('ip.txt', 'r')
+
+entries = 0
+ip_types = {'2001:0db8':0, '2001:0e':0, 'fc/fd':0}
+
+for line in input_file:
+    if line.startswith('2001:0db8'):
+        ip_types['2001:0db8']+=1
+        pass
+    elif line.startswith('2001:0e'):
+        ip_types['2001:0e']+=1
+        pass
+    elif line.startswith('fc') or line.startswith('fd'):
+        ip_types['fc/fd']+=1
+        pass
+    else:
+        continue
+    
+    entries+=1
+
+print('Adatsorok száma:', entries)
+
+for k,v in ip_types.items():
+    print('"'+str(k)+'" kezdetűek: '+str(v))
