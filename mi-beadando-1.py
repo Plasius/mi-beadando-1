2aa050ea4ca960b1a1801cd56d6286ff90b7eb5a
Plasius <Plasius@users.noreply.github.com>
2022-10-12 17:09:02 +0200

    Add files via upload

--- mi-beadando-1.py
+++ mi-beadando-1.py
@@ -7,13 +7,10 @@ ip_types = {'2001:0db8':0, '2001:0e':0, 'fc/fd':0}
 for line in input_file:
     if line.startswith('2001:0db8'):
         ip_types['2001:0db8']+=1
-        pass
     elif line.startswith('2001:0e'):
         ip_types['2001:0e']+=1
-        pass
     elif line.startswith('fc') or line.startswith('fd'):
         ip_types['fc/fd']+=1
-        pass
     else:
         continue
     
